1d45bda7bad1aea069415f4603537a62523d2fab
Aftab0Alam <aftab0alam011@gmail.com>
2025-12-03 23:17:14 +0530

    binary search

---
+++ test1.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class test1 {
+    public static void main(String[] args) {
+     int arr[]={1,3,2,99,5,4,198,7,6};
+        Arrays.sort(arr);
+        for(int i:arr){
+            System.out.println(i);
+           
+
+        }
+        }
+     System.out.println("**********");
+            int n=Arrays.binarySearch(arr,3);
+             System.out.println(n);
+            
+    }
